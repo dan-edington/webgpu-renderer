c3e7c449f1e204f0ee984e7fd934abc8ef8e3175
Dan Edington <dan2505@hotmail.com>
2026-05-15 14:54:20 +0100

    add Normal and Unlit Material demos

---
+++ dev/NormalMaterial/main.ts
@@ -0,0 +1,56 @@
+import '../style.css';
+import { sphere } from 'primitive-geometry';
+
+import { Geometry, Mesh, Renderer, Scene, PerspectiveCamera, OrbitControls, NormalMaterial } from '../../src/index';
+
+const container = document.getElementById('app');
+
+if (container) {
+  // Create and init the renderer
+  const renderer = await Renderer.create({ containerElement: container, alpha: true });
+
+  // Create a scene
+  const scene = new Scene();
+  scene.setClearColor([0.25, 0.25, 0.25, 1]);
+
+  // Create a camera
+  const camera = new PerspectiveCamera({
+    near: 0.1,
+    far: 100,
+    fov: (60 * Math.PI) / 180,
+    aspect: container.clientWidth / container.clientHeight,
+  });
+
+  // Create cube geometry
+  const spherePrimitive = sphere({ radius: 1, nx: 32, ny: 32 });
+  const sphereGeometry = new Geometry({
+    vertices: spherePrimitive.positions,
+    indices: Uint16Array.from(spherePrimitive.cells),
+    normals: spherePrimitive.normals,
+    uvs: spherePrimitive.uvs,
+  });
+
+  const normalMaterial = new NormalMaterial();
+
+  const sphereMesh = new Mesh(sphereGeometry, normalMaterial);
+
+  // Add objects to scene
+  scene.add([sphereMesh]);
+
+  camera.setPosition(0, 0, 5);
+  camera.lookAt(new Float32Array([0, 0, 0]));
+  new OrbitControls({ camera, domElement: renderer.surfaceManager.canvasElement });
+
+  // Render the scene
+  function render() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+
+  render();
+
+  // Add resize handler for camera
+  window.addEventListener('resize', () => {
+    camera.aspect = container.clientWidth / container.clientHeight;
+  });
+}
